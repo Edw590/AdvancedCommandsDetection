144f19d44eeed6e217bc8d82c06c130f8d5f70f0
DADi590 <40151365+DADi590@users.noreply.github.com>
2021-11-21 23:24:23 +0000

    Optimize: size of generated AAR for Android
    1.1;
    ===
    List of changes:
    - Optimize: size of generated AAR for Android.
      With NLP on the project, the size jumped from 4.9 MB to 25.5 MB, so I
      added 2 flags to reduce the size a bit (the other flags are just to
      print build information). This made the AAR package go to 22.1 MB. Not
      that much, but that's the best that can come out, I guess. As it's not
      a binary file, UPX can't be used...
    
    ===

--- Build_AAR_Android.bat
+++ Build_AAR_Android.bat
@@ -7,7 +7,13 @@ CD %1
 :: Only put here below the packages that need the exported names visible.
 :: The others will be added automatically (if needed?).
 :: Keep GlobalUtils here because it has the VERSION constant, which is useful to know.
-gomobile bind --target=android -o "GeneratedBinaries_EOG/assist_platforms_unifier.aar"^
+:: Also keep compressdwarf here. It's default true, but they could change it to default false, so this way it's true for
+:: sure.
+gomobile bind^
+ --target=android^
+ -x^
+ -ldflags="-v -s -w -compressdwarf=true"^
+ -o="GeneratedBinaries_EOG/assist_platforms_unifier.aar"^
  "Assist_Platforms_Unifier/APU_GlobalUtils"^
  "Assist_Platforms_Unifier/APU_CmdDetection"
 
