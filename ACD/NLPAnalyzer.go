03c0d3cd34916e01f460d925c4da28213d897ea5
Edw590 <40151365+Edw590@users.noreply.github.com>
2024-06-12 09:36:08 +0100

    Remove useless TODO

--- ACD/NLPAnalyzer.go
+++ ACD/NLPAnalyzer.go
@@ -1,18 +1,23 @@
-/*
- * Copyright 2021 DADi590
+/*******************************************************************************
+ * Copyright 2023-2024 Edw590
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ ******************************************************************************/
 
 package ACD
 
@@ -134,9 +139,6 @@ func nlpAnalyzer(sentence *[]string, sentence_str string, nlp_meanings []string)
 
 	resetVariables()
 
-	// todo See how this file works and finish this. You were going to pass to the function an it_and variable saying
-	//  what was "it" and what "and" was referring to, like "turn wifi on", and seconds later, "turn it off" --> "wifi"
-	//  is the "it"
 	//nlp_last_name_found = append(nlp_last_name_found, it_and)
 
 	//log.Println("-----------------------------")
